870740c5f33fb0a6febd7ac05dfe174bc679f7c4
Suyash <mishrasuyash2018@gmail.com>
2025-07-12 23:19:40 +0530

    Added a home-page down scroll indicator arrow

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import "./style.css";
 import Navbar from "../pages/navbar.jsx";
 import BgBlurredCircles from "../pages/BgBlurredCircles.jsx";
 import { ReactLenis, useLenis } from "lenis/react";
+import { IoIosArrowDown } from "react-icons/io";
+
 
 const App = () => {
   const lenis = useLenis((lenis) => {
@@ -14,6 +16,9 @@ const App = () => {
     <ReactLenis root smooth duration={1.2}>
       <Navbar />
       <BgBlurredCircles />
+      <div className="flex justify-center absolute left-1/2 -translate-x-1/2 bottom-10">
+        <IoIosArrowDown className="text-4xl text-white" />
+      </div>
     </ReactLenis>
   );
 };
