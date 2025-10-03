3101558ee5cc9986e18bcbf232a98241c9c33a11
Suyash <mishrasuyash2018@gmail.com>
2025-10-04 00:12:19 +0530

    attempt#2 to fix deployment build

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import Lottie from "lottie-react";
 import "./style.css";
 import Navbar from "./pages/navbar.jsx";
 import BgBlurredCircles from "./pages/BgBlurredCircles.jsx";
-import { ReactLenis, useLenis } from "lenis/react";
+// import { ReactLenis, useLenis } from "lenis/react";
 import { IoIosArrowDown } from "react-icons/io";
 import { GoSun } from "react-icons/go";
 import Flow1 from "./assets/Flow1.json"; // Your Lottie JSON data
 
 const App = () => {
-  const lenis = useLenis((lenis) => {
-    console.log(lenis);
-  });
+  // const lenis = useLenis((lenis) => {
+  //   // console.log(lenis);
+  // });
 
   // NOTE: With lottie-react, you **do not** need the defaultOptions object.
   // Properties like loop, autoplay, and animationData are passed directly as props.
@@ -20,11 +20,12 @@ const App = () => {
   };
 
   return (
-    <ReactLenis root smooth duration={1.2}>
+    // <ReactLenis root smooth duration={1.2}>
+    <>
       <BgBlurredCircles />
       <div className="flex flex-col items-center justify-center h-screen relative z-10 text-white">
         {/* Lottie Container: Defines the maximum size and responsiveness */}
-        <div className="w-full max-w-lg md:max-w-xl lg:max-w-xl h-auto">
+        <div className="w-2xs max-w-xl md:max-w-xl lg:max-w-xl h-auto">
           <Lottie
             animationData={Flow1}
             loop={true}
@@ -34,7 +35,8 @@ const App = () => {
         </div>
         {/* <h1>Soon.</h1> */}
       </div>
-    </ReactLenis>
+    </>
+    // </ReactLenis>
   );
 };
 
