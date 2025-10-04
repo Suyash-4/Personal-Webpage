6a51cd57d87cf34734d6876c678f518b0354cd18
Suyash <mishrasuyash2018@gmail.com>
2025-10-04 12:03:03 +0530

    attempt#4

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 /*eslint-disable */
 import "./style.css";
-import Navbar from "./pages/Navbar.jsx";
+
 import BgBlurredCircles from "./pages/BgBlurredCircles.jsx";
 import { ReactLenis, useLenis } from "lenis/react";
 import { IoIosArrowDown } from "react-icons/io";
 import { GoSun } from "react-icons/go";
+import Nav from "./pages/Nav.jsx";
 
 const App = () => {
   const lenis = useLenis((lenis) => {
-    console.log(lenis);
+    // console.log(lenis);
   });
 
   return (
@@ -16,7 +17,7 @@ const App = () => {
       <>
         <BgBlurredCircles />
         <div className="flex flex-col items-center justify-center h-screen relative z-10 text-white">
-          <Navbar />
+          <Nav />
         </div>
       </>
     </ReactLenis>
